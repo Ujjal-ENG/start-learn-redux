085d7a37e145b75c787172b2f088bc93d6f91f53
user <ujjalroy7862@gmail.com>
2023-03-16 18:38:58 +0600

    findout the error from the code and fixed it

--- src/fetaures/posts/PostView.js
+++ src/fetaures/posts/PostView.js
@@ -15,7 +15,7 @@ const PostView = () => {
     return (
         <div>
             {isLoading && <h1 className="text-2xl font-bold text-red-600">Loading....</h1>}
-            {error && <h1 className="text-2xl font-bold text-red-600">Error Ocured....</h1>}
+            {error && <h1 className="text-2xl font-bold text-red-600">Error Ocured....{error}</h1>}
             {post &&
                 post.map((el) => {
                     return (
