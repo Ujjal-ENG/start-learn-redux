e2b7773f11e4ed9bb6da7447f74334319bb705fa
user <ujjalroy7862@gmail.com>
2023-03-17 00:32:09 +0600

    done the redux part

--- src/CRUP-APP/pages/Home.js
+++ src/CRUP-APP/pages/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div>
+            {' '}
+            <h1 className="text-7xl text-violet-500 font-bold text-center">This is the Home Page!!!</h1>
+        </div>
+    );
 };
 
 export default Home;
